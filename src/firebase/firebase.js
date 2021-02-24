7bf0dd7bf52adede0edb2ba41b7417656c9e0d9e
Carlos Roberto <carlosbuanaoda@gmail.com>
2021-02-24 11:53:34 -0300

    test heroku

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase';
 import 'firebase/database';
 
+console.log("aaaaaaaaaaaaaaaaaaaaaa", firebaseConfig) 
+
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
@@ -9,6 +11,8 @@ const firebaseConfig = {
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
 };
+
+
 
 firebase.initializeApp(firebaseConfig);
 

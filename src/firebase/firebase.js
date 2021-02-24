8ab94145cfcb4fb2c10588d3f17676b8e236e528
Carlos Roberto <carlosbuanaoda@gmail.com>
2021-02-24 12:41:19 -0300

    test heroku

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,22 @@
 import firebase from 'firebase';
 import 'firebase/database';
 
+// const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.FIREBASE_DATABASE_URL,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  apiKey: "AIzaSyBdJBp47fQzwJULn2b0HJS9ocf6bnzBbsw",
+  authDomain: "kombucha-sao-pio.firebaseapp.com",
+  databaseURL: "https://kombucha-sao-pio-default-rtdb.firebaseio.com",
+  projectId: "kombucha-sao-pio",
+  storageBucket: "kombucha-sao-pio.appspot.com",
+  messagingSenderId: "337256765143",
 };
 
 console.log("FirebaseConfig: ", firebaseConfig)
